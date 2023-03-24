23141ee4b8af11639e7b5aeafd2520490bc6cf31
NySandratra <122896188+NySandratra1@users.noreply.github.com>
2023-03-24 05:39:22 +0200

    sprint3

--- Mapping.java
+++ Mapping.java
@@ -1,6 +1,23 @@
 package etu2018.framework;
 
 public class Mapping{
-    String classe;
-    String method;
+    private String classe;
+    private String method;
+
+    public String getClasse(){
+        return classe;
+    }
+
+    public String getMethod(){
+        return method;
+    }
+
+    public void setClasse(String classe){
+        this.classe = classe;
+    }
+
+    public void setMethod(String method){
+        this.method  = method;
+    }
+
 }
